201f3da544438217535d347e25a934c218829cc2
Regis Andrade <regis.batista.andrade@outlook.com>
2025-05-20 22:24:16 -0300

    APPLICATION - CodeBase: UI Finished and starting implementation rules in  ...xaml.cs

--- APPLICATION/MainWindow.xaml.cs
+++ APPLICATION/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,107 @@ namespace APPLICATION
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _timerText;
+        private TimeSpan _timer;
+        private int potential;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #region Events
+
+        private void Border_Sair_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        #region Numpad/Keyboard
+
+        private void Border_Key_One_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_Key_Two_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_Key_Three_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_Key_Four_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_Key_Five_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_Key_Six_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_Key_Seven_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_Key_Eight_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_Key_Nine_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_Key_Zero_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        #endregion Numpad/Keyboard
+
+        #region Actions Keys
+
+        private void Border_Key_LessPower_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_Key_MorePower_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+
+        private void Border_ActionKey_Pause_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Border_ActionKey_Start_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        #endregion Actions Keys
+
+        #endregion Events
+
+        private void Set_Timer_Text(string value)
+        {
+
+        }
+
     }
 }
